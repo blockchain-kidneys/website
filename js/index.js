e6459ca7d2818231e8269e1ac8ec76ba2bb597a8
mauriciofs <mauriciofs6@gmail.com>
2015-11-07 18:54:23 +0000

    Adding confirmation page

--- js/index.js
+++ js/index.js
@@ -40,30 +40,7 @@ $(".next").click(function(){
 	
 	//activate next step on progressbar using the index of next_fs
 	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'transform': 'scale('+scale+')'});
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
+	changePage(current_fs, true, false, next_fs);
 });
 
 $(".previous").click(function(){
@@ -75,30 +52,8 @@ $(".previous").click(function(){
 	
 	//de-activate current step on progressbar
 	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+	changePage(current_fs, false, previous_fs, false);
 	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
 });
 
 $(".submit").on('click', function(event){
@@ -120,7 +75,56 @@ $(".submit").on('click', function(event){
 		/*var result = api(url, data, function(data){
 			//
 		});*/
+		
+		//Show confirmation page
+		current_fs = $(this).parent();
+		next_fs = $(this).parent().next();
+		changePage(current_fs, true, false, next_fs);
 	}
 
 	//Show any error message
 });
+
+/**
+ * Change page using fieldset (show|hide)
+ * @param  object     currentPage  Current Element
+ * @param  boolean 	  next         If should show next page false means previous
+ * @param  object     previousPage Previous page element
+ * @return void
+ */
+function changePage(currentPage, next, previousPage, nextPage){
+	//Next is true change to next page
+	if(next){
+		nextPage.show();
+	}else{
+		previousPage.show();
+	}
+
+	//hide the current fieldset with style
+	currentPage.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = (next) ? 1 - (1 - now) * 0.2 : 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = (next) ? (now * 50)+"%" : ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			
+			if(next){
+				currentPage.css({'transform': 'scale('+scale+')'});
+				nextPage.css({'left': left, 'opacity': opacity});
+			}else{
+				previousPage.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+				currentPage.css({'left': left});
+			}
+		}, 
+		duration: 800, 
+		complete: function(){
+			currentPage.hide();
+			animating = false;
+		}, 
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+}
